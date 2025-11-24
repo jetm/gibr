aba8c1023da3c0a7adf37f3ef51833c8059c11ff
Yair Treister <ytreister@gmail.com>
2025-11-24 19:13:30 +0200

    Made gibr issues pipeable (#68)
    * Made updates
    
    * More updates
    
    * Added one more test
    
    * Added pagination
    
    * Removed failing test
    
    * Added more tests
    
    * Fixed test

--- src/gibr/cli/issues.py
+++ src/gibr/cli/issues.py
@@ -1,14 +1,25 @@
 """CLI command to list open issues from the tracker."""
 
+import json
+import sys
+from dataclasses import asdict
+
 import click
 from tabulate import tabulate
 
-from gibr.notify import warning
+from gibr.notify import safe_echo, warning
 
 
 @click.command("issues")
+@click.option(
+    "--json",
+    "output_json",
+    is_flag=True,
+    default=False,
+    help="Output issues in JSON format.",
+)
 @click.pass_context
-def issues(ctx):
+def issues(ctx, output_json: bool):
     """List open issues from the tracker."""
     tracker = ctx.obj["tracker"]
     issues = tracker.list_issues()
@@ -16,9 +27,13 @@ def issues(ctx):
         warning("No open issues found.")
         return
     table = [[issue.id, issue.type, issue.title, issue.assignee] for issue in issues]
-
-    click.echo(
-        tabulate(
+    if output_json:
+        click.echo(json.dumps([asdict(issue) for issue in issues], indent=4))
+    else:
+        output = tabulate(
             table, headers=["Issue", "Type", "Title", "Assignee"], tablefmt="github"
         )
-    )
+        if sys.stdout.isatty():
+            click.echo_via_pager(output)
+        else:
+            safe_echo(output)
